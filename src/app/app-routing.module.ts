e1f42042aa49cb26814c55bbdef03f2bca46b477
velrino <denismagalhaesvelrino@gmail.com>
2020-06-18 00:07:01 -0300

    :zap: Implemented Admin and Site Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: ':site',
+    loadChildren: () => import('./site/site.module').then(m => m.SiteModule)
+  },
+  { path: '', redirectTo: 'admin', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
